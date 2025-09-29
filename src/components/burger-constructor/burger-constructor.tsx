5e854a859f8f6a209bdae01776d95ce8d223b1f8
savdmi <savdmien@yandex.ru>
2025-09-29 11:34:35 +0300

    PR11-3 (uuid)

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -2,22 +2,34 @@ import { FC, useMemo } from 'react';
 import { TConstructorIngredient } from '@utils-types';
 import { BurgerConstructorUI } from '@ui';
 import { RootState, useDispatch, useSelector } from '../../services/store';
-import { resetOrderModalData } from '../../services/slices/orderSlice';
+import {
+  createOrder,
+  resetOrderModalData
+} from '../../services/slices/orderSlice';
 import { resetConstructor } from '../../services/slices/makerSlice';
+import { useNavigate } from 'react-router-dom';
 
 export const BurgerConstructor: FC = () => {
   const dispatch = useDispatch();
-  const orderRequest = useSelector(
-    (store: RootState) => store.order.orderRequest
-  );
-  const orderModalData = useSelector(
-    (store: RootState) => store.order.orderModalData
-  );
+  const navigate = useNavigate();
+  const orderRequest = useSelector((store) => store.order.orderRequest);
+  const orderModalData = useSelector((store) => store.order.orderModalData);
   const constructorItems = useSelector((store) => store.maker);
+  const { isAuthenticated } = useSelector((store) => store.user);
 
   const onOrderClick = () => {
     if (!constructorItems.bun || orderRequest) return;
+    if (!isAuthenticated) return navigate('/login');
+    const order = [
+      constructorItems.bun._id,
+      ...constructorItems.ingredients.map(
+        (item: TConstructorIngredient) => item._id
+      ),
+      constructorItems.bun._id
+    ];
+    dispatch(createOrder(order));
   };
+
   const closeOrderModal = () => {
     dispatch(resetOrderModalData());
     dispatch(resetConstructor());
